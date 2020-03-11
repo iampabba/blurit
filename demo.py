8f844d18e02b327c611b69af877e5f15c5991a37
bharatpabba <bharath.pabba1@gmail.com>
2020-03-11 23:59:46 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,4 @@
+from blurit import blur_eyes_in_images
+#blur_eyes_in_images('[YOUR-IMAGES-FOLDER-PATH]','[GENERATED-IMAGES-FOLDER-PATH]','VALUE-OF-HEIGHT-AND-WIDTH-OF-BLUR')
+
+blur_eyes_in_images('./images','./images_generated',15)
